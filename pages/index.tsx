816319ef1ff265cb64fc58b0cf36e12dab44445a
Ashik Arefin <abdullahilashik@gmail.com>
2023-03-01 08:31:19 +0600

    container fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        
+      <main className='container mx-auto'>
+        <h1 className='text-xl'>tailwind setup</h1>
       </main>
     </>
   )
